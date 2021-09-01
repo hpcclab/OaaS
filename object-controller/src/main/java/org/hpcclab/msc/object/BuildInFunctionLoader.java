4e11a90468f47649d777c3c5a5bac8619e52729c
Pawissanutt Lertpongrujikorn <pawissanutt@gmail.com>
2021-09-01 12:48:48 -0500

    Add k8s deployment

--- object-controller/src/main/java/org/hpcclab/msc/object/BuildInFunctionLoader.java
+++ object-controller/src/main/java/org/hpcclab/msc/object/BuildInFunctionLoader.java
@@ -2,7 +2,6 @@ package org.hpcclab.msc.object;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.runtime.StartupEvent;
-import io.smallrye.mutiny.Uni;
 import org.hpcclab.msc.object.entity.MscFunction;
 import org.hpcclab.msc.object.repository.MscFuncRepository;
 import org.slf4j.Logger;
@@ -10,16 +9,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.concurrent.CompletionStage;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BuildInFunctionLoader {
   private static final Logger LOGGER = LoggerFactory.getLogger(BuildInFunctionLoader.class);
@@ -30,34 +23,19 @@ public class BuildInFunctionLoader {
   MscFuncRepository funcRepo;
 
   CompletionStage<Void> onStart(@Observes StartupEvent ev) throws URISyntaxException, IOException {
-    ClassLoader classLoader = getClass().getClassLoader();
+    var inputStream = getClass().getResourceAsStream("/functions/buildin.logical.json");
 
-    URL resource = classLoader.getResource("/functions");
-
-    // dun walk the root path, we will walk all the classes
-    List<File> collect = Files.walk(Paths.get(resource.toURI()))
-      .filter(Files::isRegularFile)
-      .map(Path::toFile)
-      .collect(Collectors.toList());
-
-    var funcList = collect.stream()
-      .map(file -> {
-        MscFunction func = null;
-        try {
-          func = mapper.readValue(file, MscFunction.class);
-        } catch (IOException e) {
-          LOGGER.error("import build-in function fail {}", file.getName());
-          throw new RuntimeException(e);
-        }
+    var functions = mapper.readValue(inputStream,
+      MscFunction[].class);
+    var functionStream = Stream.of(functions)
+      .peek(func -> {
         if (LOGGER.isDebugEnabled()) {
           LOGGER.debug("import build-in function {}", func);
         } else {
           LOGGER.info("import build-in function {}", func.getName());
         }
-        return func;
-      })
-      .collect(Collectors.toList());
-    return funcRepo.persistOrUpdate(funcList)
+      });
+    return funcRepo.persistOrUpdate(functionStream)
       .subscribeAsCompletionStage();
   }
 }
