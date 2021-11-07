33cc53b90f4d2df1954a82d7f182dce944341edd
Pawissanutt <pawissanutt@gmail.com>
2021-11-07 01:40:54 -0500

    Add ExceptionMapper

--- util.sh
+++ util.sh
@@ -12,9 +12,9 @@ export JAVA_HOME=~/.jdks/temurin-17/
     -Dquarkus.jib.base-jvm-image=core.harbor.10.131.36.27.nip.io/proxy/library/eclipse-temurin:17-focal \
     -Dquarkus.container-image.insecure=true
 
-$ENV:JAVA_HOME="C:\Program Files\Java\jdk-17"
+$ENV:JAVA_HOME="$HOME/.jdks/temurin-17.0.1"
 $ENV:CI_REGISTRY_IMAGE="core.harbor.10.131.36.27.nip.io/oaas"
-mvn package -DskipTests "-Dquarkus.container-image.username=admin"    "-Dquarkus.container-image.password=Harbor12345"    "-Dquarkus.container-image.build=true"  "-Dquarkus.container-image.push=true"    "-Dquarkus.container-image.insecure=true" "-Dquarkus.jib.base-jvm-image=core.harbor.10.131.36.27.nip.io/proxy/library/eclipse-temurin:17-alpine"
+mvn package -DskipTests "-Dquarkus.container-image.username=admin"    "-Dquarkus.container-image.password=Harbor12345"    "-Dquarkus.container-image.build=true"  "-Dquarkus.container-image.push=true"    "-Dquarkus.container-image.insecure=true" "-Dquarkus.jib.base-jvm-image=core.harbor.10.131.36.27.nip.io/proxy/library/eclipse-temurin:17-focal"
 
 NAMESPACE="msc"
 kubectl -n $NAMESPACE apply -f kafka -f ksql
