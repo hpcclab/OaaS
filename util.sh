7cc76b0f84fb2a3eea25a93d05ba1b5a45d96a72
Pawissanutt <pawissanutt@gmail.com>
2021-12-29 04:10:16 -0600

    Using temurin:17-jre-alpine

--- util.sh
+++ util.sh
@@ -9,7 +9,7 @@ export JAVA_HOME=~/.jdks/temurin-17/
     -Dquarkus.container-image.build=true \
     -Dquarkus.container-image.push=true \
     -Dquarkus.jib.always-cache-base-image=true \
-    -Dquarkus.jib.base-jvm-image=core.harbor.10.131.36.27.nip.io/proxy/library/eclipse-temurin:17-focal \
+    -Dquarkus.jib.base-jvm-image=core.harbor.10.131.36.27.nip.io/proxy/library/eclipse-temurin:17-jre-alpine \
     -Dquarkus.container-image.insecure=true
 
 $ENV:JAVA_HOME="$HOME/.jdks/temurin-17.0.1"
