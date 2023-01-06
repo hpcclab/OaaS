0e5394db87772ec149602487a6d9ed419b2dd7a3
Pawissanutt <pawissanutt@gmail.com>
2023-01-05 21:56:46 -0600

    Update concat function

--- example/functions/concat-function/src/main/java/org/hpcclab/oaas/funqy/cloudevent/ConcatHandler.java
+++ example/functions/concat-function/src/main/java/org/hpcclab/oaas/funqy/cloudevent/ConcatHandler.java
@@ -13,6 +13,7 @@ import io.vertx.mutiny.core.buffer.Buffer;
 import io.vertx.mutiny.ext.web.client.HttpResponse;
 import io.vertx.mutiny.ext.web.client.WebClient;
 import io.vertx.mutiny.ext.web.client.predicate.ResponsePredicate;
+import org.hpcclab.oaas.model.object.ObjectUpdate;
 import org.hpcclab.oaas.model.task.OaasTask;
 import org.hpcclab.oaas.model.task.TaskCompletion;
 import org.slf4j.Logger;
@@ -21,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Set;
 
 @ApplicationScoped
 public class ConcatHandler {
@@ -45,6 +47,11 @@ public class ConcatHandler {
   public Uni<CloudEvent<TaskCompletion>> handle(OaasTask task, @Context CloudEvent<OaasTask> event) {
     var args = task.getOutput().getOrigin().getArgs();
     var inputUrl = task.getMainKeys().get("text");
+    var tc = new TaskCompletion()
+      .setId(task.getOutput().getId())
+      .setVId(task.getVId())
+      .setSuccess(true)
+      .setOutput(new ObjectUpdate().setUpdatedKeys(Set.of("text")));
     return getText(inputUrl)
       .flatMap(text -> {
         var append = args.getOrDefault("APPEND", "");
@@ -55,11 +62,7 @@ public class ConcatHandler {
         .id(task.getId())
         .type("oaas.task.result")
         .source("oaas/concat")
-        .build(new TaskCompletion()
-          .setId(task.getOutput().getId())
-          .setVId(task.getVId())
-          .setSuccess(true)
-        )
+        .build(tc)
       );
   }
 
