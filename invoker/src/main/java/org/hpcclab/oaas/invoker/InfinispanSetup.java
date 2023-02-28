61da7874a7fdbe00d62967f1bff387a833a16479
Pawissanutt <pawissanutt@gmail.com>
2023-02-28 03:27:20 -0600

    Fix the cannot join cluster issue via changing ping service

--- invoker/src/main/java/org/hpcclab/oaas/invoker/InfinispanSetup.java
+++ invoker/src/main/java/org/hpcclab/oaas/invoker/InfinispanSetup.java
@@ -1,25 +1,35 @@
 package org.hpcclab.oaas.invoker;
 
+import io.quarkus.cache.CacheManager;
 import io.quarkus.runtime.StartupEvent;
 import io.vertx.mutiny.core.Vertx;
 import org.infinispan.api.Infinispan;
+import org.infinispan.commons.dataconversion.MediaType;
+import org.infinispan.commons.util.FileLookupFactory;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
 import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 import java.util.UUID;
 
 @ApplicationScoped
 public class InfinispanSetup {
   @Inject
   Vertx vertx;
+  EmbeddedCacheManager cacheManager;
   private static final Logger logger = LoggerFactory.getLogger(InfinispanSetup.class);
 
   public void setup(
@@ -29,36 +39,26 @@ public class InfinispanSetup {
     var dns = System.getenv("ISPN_DNS_PING");
     if (dns!=null) {
       logger.info("use dns.query {}", dns);
-//      System.setProperty("infinispan.cluster.stack", "kubernetes");
       System.setProperty("jgroups.dns.query", dns);
     }
     var podName = System.getenv("ISPN_POD_NAME");
     if (podName==null) {
       podName = "invoker-" + UUID.randomUUID();
     }
-    logger.info("use nodeName {}", podName);
+    System.setProperty("infinispan.node.name", podName);
 
 
-    GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
-
-    global.defaultCacheName("defaultCache")
-      .transport()
-      .defaultTransport()
-      .clusterName("oaas-invoker")
-      .nodeName(podName)
-      .addProperty("configurationFile", "default-configs/default-jgroups-kubernetes.xml")
-    ;
-
-    Configuration cacheConfiguration = new ConfigurationBuilder()
-      .clustering()
-      .cacheMode(CacheMode.REPL_SYNC)
-      .build();
-
-    // Initialize the default Cache Manager.
-    var conf = global.build();
-    DefaultCacheManager cacheManager = new DefaultCacheManager(global.build(), cacheConfiguration);
-    logger.info("starting infinispan {}", conf);
-    cacheManager.start();
-    logger.info("started infinispan");
+    try {
+      InputStream configurationStream = FileLookupFactory.newInstance().lookupFileStrict("infinispan.xml",
+        Thread.currentThread().getContextClassLoader());
+      ConfigurationBuilderHolder configHolder = new ParserRegistry().parse(configurationStream, null, MediaType.APPLICATION_XML);
+      configHolder.getGlobalConfigurationBuilder()
+          .security();
+      logger.info("starting infinispan {}", configHolder);
+      cacheManager = new DefaultCacheManager(configHolder, true);
+      logger.info("started infinispan");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
