7bd92f386ab4deb16ca463ee170c0ca1411ee65e
Pawissanutt <pawissanutt@gmail.com>
2023-04-24 05:30:45 -0500

    Allow reusing http session

--- example/functions/concat-fn-py/main.py
+++ example/functions/concat-fn-py/main.py
@@ -1,6 +1,4 @@
 import logging
-import random
-import string
 import time
 
 import aiohttp
@@ -20,16 +18,16 @@ class ConcatHandler(oaas.Handler):
         record = ctx.task.main_obj.data.copy() if ctx.task.main_obj.data is not None else {}
 
         record['ts'] = round(time.time() * 1000)
-        resp = await ctx.load_main_file("text")
-        old_text = await resp.read()
-        text = old_text.decode("utf-8") + append
-        b_text = bytes(text, 'utf-8')
 
         async with aiohttp.ClientSession() as session:
-            if inplace:
-                await ctx.upload_main_byte_data(session, "text", b_text)
-            else:
-                await ctx.upload_byte_data(session, "text", b_text)
+            async with await ctx.load_main_file(session, "text") as resp:
+                old_text = await resp.read()
+                text = old_text.decode("utf-8") + append
+                b_text = bytes(text, 'utf-8')
+                if inplace:
+                    await ctx.upload_main_byte_data(session, "text", b_text)
+                else:
+                    await ctx.upload_byte_data(session, "text", b_text)
 
 
 app = FastAPI()
@@ -44,6 +42,7 @@ async def handle(request: Request):
     resp = await router.handle_task(body)
     logging.debug(f"completion {resp}")
     return resp
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
