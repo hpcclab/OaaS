d35788bb5f4112fa9b03f990c1c4b455bb293ad9
Pawissanutt <pawissanutt@gmail.com>
2023-04-24 16:04:14 -0500

    Add object construction API to invoker

---
+++ invoker/src/main/java/org/hpcclab/oaas/invoker/rest/ObjectConstructResource.java
@@ -0,0 +1,115 @@
+package org.hpcclab.oaas.invoker.rest;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.hpcclab.oaas.invoker.service.DataAllocationService;
+import org.hpcclab.oaas.model.Views;
+import org.hpcclab.oaas.model.cls.OaasClass;
+import org.hpcclab.oaas.model.data.DataAllocateRequest;
+import org.hpcclab.oaas.model.data.DataAllocateResponse;
+import org.hpcclab.oaas.model.exception.NoStackException;
+import org.hpcclab.oaas.model.object.OaasObject;
+import org.hpcclab.oaas.model.object.ObjectConstructRequest;
+import org.hpcclab.oaas.model.object.ObjectConstructResponse;
+import org.hpcclab.oaas.repository.ClassRepository;
+import org.hpcclab.oaas.repository.OaasObjectFactory;
+import org.hpcclab.oaas.repository.ObjectRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@ApplicationScoped
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/api/object-construct")
+public class ObjectConstructResource {
+
+  @Inject
+  ClassRepository clsRepo;
+  @Inject
+  ObjectRepository objRepo;
+  @Inject
+  DataAllocationService allocationService;
+  @Inject
+  OaasObjectFactory objectFactory;
+
+
+  @POST
+  @JsonView(Views.Public.class)
+  public Uni<ObjectConstructResponse> construct(ObjectConstructRequest construction) {
+    var cls = clsRepo.get(construction.getCls());
+    if (cls==null) throw NoStackException.notFoundCls400(construction.getCls());
+    return switch (cls.getObjectType()) {
+      case SIMPLE, COMPOUND -> constructSimple(construction, cls);
+      case STREAM -> constructStream(construction, cls);
+    };
+  }
+
+  private void linkReference(ObjectConstructRequest request,
+                             OaasObject obj,
+                             OaasClass cls) {
+    //TODO validate the references of request
+    obj.setRefs(request.getRefs());
+  }
+
+  private Uni<ObjectConstructResponse> constructSimple(ObjectConstructRequest construction,
+                                                       OaasClass cls) {
+    var obj = objectFactory.createBase(construction, cls);
+    linkReference(construction, obj, cls);
+    var stateSpec = cls.getStateSpec();
+    if (stateSpec==null) return objRepo.persistAsync(obj)
+      .map(ignore -> new ObjectConstructResponse(obj, Map.of()));
+
+    var ks = Lists.fixedSize.ofAll(cls.getStateSpec().getKeySpecs())
+      .select(k -> construction.getKeys().contains(k.getName()));
+    if (ks.isEmpty()) {
+      return objRepo.persistAsync(obj)
+        .map(ignored -> new ObjectConstructResponse(obj, Map.of()));
+    }
+    DataAllocateRequest request = new DataAllocateRequest(obj.getId(), ks, cls.getStateSpec().getDefaultProvider(), true);
+    return allocationService.allocate(List.of(request))
+      .map(list -> new ObjectConstructResponse(obj, list.get(0).getUrlKeys()))
+      .call(() -> objRepo.persistAsync(obj));
+  }
+
+  private Uni<ObjectConstructResponse> constructStream(ObjectConstructRequest construction,
+                                                       OaasClass cls) {
+    var genericType = cls.getGenericType();
+    var genericCls = clsRepo.get(genericType);
+    var obj = objectFactory.createBase(construction, cls);
+    linkReference(construction, obj, cls);
+    var sc = Lists.fixedSize.ofAll(construction.getStreamConstructs());
+    var objStream = sc.collectWithIndex((c,i) -> objectFactory.createBase(c, genericCls, obj.getId() + '.' + i));
+    var requestList = sc.zip(objStream).collect(pair -> {
+      var ks = Lists.fixedSize.ofAll(cls.getStateSpec().getKeySpecs())
+        .select(k -> construction.getKeys().contains(k.getName()));
+      return new DataAllocateRequest(pair.getTwo().getId(), ks, true);
+    });
+
+    return allocationService.allocate(requestList)
+      .map(list -> aggregateResult(obj, objStream, list))
+      .call(() -> {
+        objStream.add(obj);
+        return objRepo.persistAsync(objStream);
+      });
+  }
+
+  private ObjectConstructResponse aggregateResult(OaasObject baseObj,
+                                                  MutableList<OaasObject> objStream,
+                                                  List<DataAllocateResponse> responses) {
+    var respStream = objStream.zip(responses)
+      .collect(pair -> new ObjectConstructResponse(pair.getOne(), pair.getTwo().getUrlKeys()));
+    return new ObjectConstructResponse(baseObj, Map.of(), respStream);
+  }
+
+}
---
+++ invoker/src/test/java/org/hpcclab/oaas/invoker/ObjectConstructResourceTest.java
@@ -0,0 +1,59 @@
+package org.hpcclab.oaas.invoker;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.json.Json;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Sets;
+import org.hpcclab.oaas.model.object.ObjectConstructRequest;
+import org.hpcclab.oaas.repository.ClassRepository;
+import org.hpcclab.oaas.repository.FunctionRepository;
+import org.hpcclab.oaas.repository.ObjectRepository;
+import org.hpcclab.oaas.test.MockupData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+@QuarkusTest
+@QuarkusTestResource(ArangoResource.class)
+public class ObjectConstructResourceTest {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ObjectConstructResourceTest.class);
+
+  @Inject
+  ObjectRepository objectRepo;
+  @Inject
+  ClassRepository clsRepo;
+  @Inject
+  FunctionRepository fnRepo;
+
+  @BeforeEach
+  public void setUp() {
+    MockupData.persistMock(objectRepo, clsRepo, fnRepo);
+  }
+
+  @Test
+  public void testSimple() {
+    var req = new ObjectConstructRequest()
+      .setCls(MockupData.CLS_1.getKey())
+      .setStreamConstructs(Lists.fixedSize.of(
+        new ObjectConstructRequest()
+          .setKeys(Sets.fixedSize.of("k1"))
+      ));
+    given()
+      .when()
+      .body(Json.encodePrettily(req))
+      .contentType(MediaType.APPLICATION_JSON)
+      .post("/api/object-construct")
+      .then()
+      .log().ifValidationFails()
+      .contentType(MediaType.APPLICATION_JSON)
+      .statusCode(200)
+      .body("object.state.verIds.test", notNullValue());
+  }
+}
