0036b97d8163c0e2a7b47159798828f228acc33e
Pawissanutt <pawissanutt@gmail.com>
2024-01-19 13:48:08 -0600

    Remove legacy provisioner

--- orbit-manager/src/main/java/org/hpcclab/oaas/orbit/OrbitManagerConfig.java
+++ orbit-manager/src/main/java/org/hpcclab/oaas/orbit/OrbitManagerConfig.java
@@ -3,14 +3,8 @@ package org.hpcclab.oaas.orbit;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 
-@ConfigMapping(prefix = "oaas.kp", namingStrategy = ConfigMapping.NamingStrategy.VERBATIM)
+@ConfigMapping(prefix = "oaas.om", namingStrategy = ConfigMapping.NamingStrategy.VERBATIM)
 public interface OrbitManagerConfig {
   boolean exposeKnative();
-
-  String fnProvisionTopic();
-  String clsProvisionTopic();
-
-  @WithDefault("oaas-invoke-")
-  String invokeTopicPrefix();
   String LABEL_KEY = "oaas.function";
 }
--- orbit-manager/src/main/java/org/hpcclab/oaas/orbit/provisioner/Provisioner.java
+++
@@ -1,7 +0,0 @@
-package org.hpcclab.oaas.orbit.provisioner;
-
-import java.util.function.Consumer;
-
-public interface Provisioner<T,R> {
-  R provision(T functionRecord);
-}
