f3e1219a930c7ef3020a675c97b1d45bb7a53e50
Pawissanutt <pawissanutt@gmail.com>
2024-03-21 02:35:07 -0500

    Add cli build

--- cli/src/main/java/org/hpcclab/oprc/cli/command/pkg/PackageDeleteCommand.java
+++ cli/src/main/java/org/hpcclab/oprc/cli/command/pkg/PackageDeleteCommand.java
@@ -1,14 +1,11 @@
 package org.hpcclab.oprc.cli.command.pkg;
 
-import com.jayway.jsonpath.JsonPath;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.mutiny.ext.web.client.WebClient;
 import io.vertx.mutiny.uritemplate.UriTemplate;
 import io.vertx.mutiny.uritemplate.Variables;
 import jakarta.inject.Inject;
-import org.hpcclab.oprc.cli.conf.ConfigFileManager;
-import org.hpcclab.oprc.cli.mixin.CommonOutputMixin;
-import org.hpcclab.oprc.cli.service.OutputFormatter;
 import org.hpcclab.oprc.cli.service.WebRequester;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +14,9 @@ import picocli.CommandLine;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -37,22 +36,32 @@ public class PackageDeleteCommand implements Callable<Integer> {
   public Integer call() throws Exception {
 
     var pkg = Files.readString(pkgFile.toPath());
+    YAMLMapper yamlMapper = new YAMLMapper();
+    var map = yamlMapper.readValue(pkg, Map.class);
+    JsonObject pkgJson = new JsonObject(map);
 
-    String pkgName = JsonPath.read(pkg, "$.name");
-    List<String> clsNameList = JsonPath.read(pkg, "$.classes[*].name");
+    String pkgName = pkgJson.getString("name");
+    List<String> clsNameList = new ArrayList<>();
+    JsonArray classes = pkgJson.getJsonArray("classes");
+    for (int i = 0; i < classes.size(); i++) {
+      clsNameList.add(classes.getJsonObject(i).getString("name"));
+    }
 
     for (String cls : clsNameList) {
       String clsKey = pkgName + '.' + cls;
-      System.out.println("deleting class '" + clsKey + "'...");
-      deleteCls(clsKey);
-      System.out.println("class '" + clsKey + "' is deleted");
+      System.out.print("deleting class '" + clsKey + "'...");
+      var js = deleteCls(clsKey);
+      if (js != null)
+        System.out.println("succeed");
+      else
+        System.out.println("failed");
     }
     return 0;
   }
 
 
-  public void deleteCls(String key) throws IOException {
-    webRequester.pmDelete(
+  public JsonObject deleteCls(String key) throws IOException {
+    return webRequester.pmDelete(
       UriTemplate.of("/api/classes/{+cls}")
         .expandToString(Variables.variables()
           .set("cls", key)
