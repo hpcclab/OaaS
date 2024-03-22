623355fd50321595b1ab128bfbf50b7f88a15aad
Pawissanutt <pawissanutt@gmail.com>
2024-03-22 18:15:39 -0500

    Update example fn

--- example/functions/img-resize-fn-py/config.py
+++ example/functions/img-resize-fn-py/config.py
@@ -1,7 +1,6 @@
-import multiprocessing
-from os import environ
+import os
 
-bind = '0.0.0.0:' + environ.get('PORT', '8080')
-workers = int(environ.get('WORKERS', '4'))
+bind = '0.0.0.0:' + os.environ.get('PORT', '8080')
+workers = int(os.environ.get('WORKERS', '4'))
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s ms'
 accesslog = '-'
