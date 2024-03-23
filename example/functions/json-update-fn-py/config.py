bc5f70159d065e9a40bc472e71bca5ffcc4f2b4b
Pawissanutt <pawissanutt@gmail.com>
2024-03-22 19:31:08 -0500

    Fix http close

--- example/functions/json-update-fn-py/config.py
+++ example/functions/json-update-fn-py/config.py
@@ -1,6 +1,8 @@
+import math
 import os
 
 bind = '0.0.0.0:' + os.environ.get('PORT', '8080')
 workers = int(os.environ.get('WORKERS', '4'))
 access_log_format = '%(h)s "%(r)s" %(s)s %(b)s "%(f)s" %(D)s µs'
 accesslog = '-'
+keep_alive_max_requests = math.inf
