b42cfa9cdc572656c04eb49f5a4dd8950282889a
Pawissanutt <pawissanutt@gmail.com>
2024-03-25 15:17:26 -0500

    Fix resource estimation code that only calculate Deployment

--- class-runtime-manager/src/main/java/org/hpcclab/oaas/crm/controller/ApplyK8SCrOperation.java
+++ class-runtime-manager/src/main/java/org/hpcclab/oaas/crm/controller/ApplyK8SCrOperation.java
@@ -1,18 +1,22 @@
 package org.hpcclab.oaas.crm.controller;
 
-import io.fabric8.kubernetes.api.model.Container;
-import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.api.model.ResourceRequirements;
+import io.fabric8.knative.serving.v1.RevisionTemplateSpec;
+import io.fabric8.knative.serving.v1.Service;
+import io.fabric8.knative.serving.v1.ServiceSpec;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentSpec;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.eclipse.collections.api.factory.Lists;
 import org.hpcclab.oaas.crm.env.OprcEnvironment.EnvResource;
 import org.hpcclab.oaas.crm.exception.CrDeployException;
+import org.hpcclab.oaas.proto.KnativeProvision;
 import org.hpcclab.oaas.proto.OClassStatusUpdate;
 import org.hpcclab.oaas.proto.OFunctionStatusUpdate;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class ApplyK8SCrOperation implements CrOperation {
   KubernetesClient client;
@@ -42,21 +46,44 @@ public class ApplyK8SCrOperation implements CrOperation {
   @Override
   public EnvResource estimate() {
     return k8sResources.stream()
-      .filter(Deployment.class::isInstance)
-      .map(d -> ((Deployment) d).getSpec()
-        .getTemplate()
-        .getSpec()
-        .getContainers()
-        .stream()
+      .map(this::estimate)
+      .reduce(EnvResource.ZERO, EnvResource::sum);
+  }
+
+  public EnvResource estimate(HasMetadata hasMetadata) {
+    if (hasMetadata instanceof Deployment deployment) {
+      return Optional.of(deployment)
+        .map(Deployment::getSpec)
+        .map(DeploymentSpec::getTemplate)
+        .map(PodTemplateSpec::getSpec)
+        .stream().flatMap(ps -> ps.getContainers().stream())
         .map(Container::getResources)
         .filter(Objects::nonNull)
         .map(ResourceRequirements::getRequests)
         .filter(Objects::nonNull)
         .map(EnvResource::new)
         .reduce(EnvResource.ZERO, EnvResource::sum)
-        .mul(((Deployment) d).getSpec().getReplicas())
-      )
-      .reduce(EnvResource.ZERO, EnvResource::sum);
+        .mul(deployment.getSpec().getReplicas());
+    } else if (hasMetadata instanceof Service kservice) {
+      int replica = 1;
+      var min = kservice.getMetadata().getAnnotations()
+        .get("autoscaling.knative.dev/minScale");
+      if (min != null) replica = Math.max(1, Integer.parseInt(min));
+      return Optional.of(kservice)
+        .map(Service::getSpec)
+        .map(ServiceSpec::getTemplate)
+        .map(RevisionTemplateSpec::getSpec)
+        .stream().flatMap(ps -> ps.getContainers().stream())
+        .map(Container::getResources)
+        .filter(Objects::nonNull)
+        .map(ResourceRequirements::getRequests)
+        .filter(Objects::nonNull)
+        .map(EnvResource::new)
+        .reduce(EnvResource.ZERO, EnvResource::sum)
+        .mul(replica);
+    } else {
+      return EnvResource.ZERO;
+    }
   }
 
   @Override
