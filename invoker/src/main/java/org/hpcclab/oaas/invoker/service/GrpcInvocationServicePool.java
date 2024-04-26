09ec54473f8fdb3c9524702430ddaed90bb91ff6
Pawissanutt <pawissanutt@gmail.com>
2024-04-26 17:03:18 -0500

    Make gRPC use direct executor

--- invoker/src/main/java/org/hpcclab/oaas/invoker/service/GrpcInvocationServicePool.java
+++ invoker/src/main/java/org/hpcclab/oaas/invoker/service/GrpcInvocationServicePool.java
@@ -13,22 +13,27 @@ import java.util.Map;
  * @author Pawissanutt
  */
 public class GrpcInvocationServicePool {
-  Map<CrHash.ApiAddress, InvocationService> invocationServiceMap = new ConcurrentHashMap<>();
+  Map<ServiceAddr, InvocationService> invocationServiceMap = new ConcurrentHashMap<>();
 
 
 
   public InvocationService getOrCreate(CrHash.ApiAddress addr) {
-    CrHash.ApiAddress noTsAddr = addr.toBuilder().ts(0).build();
-    return invocationServiceMap.computeIfAbsent(noTsAddr, k -> {
+    ServiceAddr serviceAddr = new ServiceAddr(addr.host(), addr.port());
+    return invocationServiceMap.computeIfAbsent(serviceAddr, k -> {
       ManagedChannelBuilder<?> builder = ManagedChannelBuilder
         .forAddress(addr.host(), addr.port())
         .disableRetry()
-        .usePlaintext();
+        .usePlaintext()
+        .directExecutor();
       return new InvocationServiceClient(addr.toString(), builder.build(), this::configure);
     });
   }
 
   MutinyInvocationServiceStub configure(String key, MutinyInvocationServiceStub stub) {
     return stub;
+  }
+
+  record ServiceAddr(String host, int port) {
+
   }
 }
